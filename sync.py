c26ef6a7963379cd7c7aa0dd044c12ca89f729fc
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2019-05-11 14:32:56 +0200

    remove sync.py

--- sync.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def sync():
-    subprocess.call(["rsync", "-ravzhe", "ssh", "/home/pi/rpi-weather/", "weather-02:/home/pi/rpi-weather", "--delete"])
-
-
-if __name__ == '__main__':
-    sync()
