dba91a768070b72656ce2a82d5c44374bcd996ba
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2019-05-11 15:45:00 +0200

    add pin number to arguments

--- am2302.py
+++ am2302.py
@@ -1,8 +1,8 @@
 import Adafruit_DHT
 
 
-def get_data():
-    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+def get_data(pin):
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, pin)
     result = {"temperature": float('{0:0.1f}'.format(temperature)),
               "humidity": int('{0:0.0f}'.format(humidity))}
 
