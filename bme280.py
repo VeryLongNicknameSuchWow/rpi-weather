76c6c521415c798144c403347a5c7e92783a9e0e
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2019-05-11 15:45:14 +0200

    add i2c address to arguments

--- bme280.py
+++ bme280.py
@@ -3,9 +3,9 @@ import busio
 import adafruit_bme280
 
 
-def get_data():
+def get_data(address):
     i2c = busio.I2C(board.SCL, board.SDA)
-    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=address)
 
     result = {"temperature": float('{0:0.1f}'.format(bme280.temperature)),
               "humidity": int('{0:0.0f}'.format(bme280.humidity)),
